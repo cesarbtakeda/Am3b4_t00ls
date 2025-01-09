75721aecd3bf10f284baac2ef2b6e5cf2a17320f
Cesar Augusto B <114227831+cesarbtakeda@users.noreply.github.com>
2025-01-09 08:57:43 -0300

    Update am3b4_t00ls.sh

--- am3b4_t00ls.sh
+++ am3b4_t00ls.sh
@@ -11,7 +11,7 @@ url=""
 api=""
 diretorio=""
 https_url=""
-
+$c="clear"
 
 # Entrada do usuário
 read -p "Qual caminho da pasta deseja salvar?: " diretorio
@@ -20,7 +20,7 @@ mkdir -p "$diretorio" || { echo "Erro ao criar o diretório. Saindo."; exit 1; }
 read -p "Sua API do WPScan: " api
 read -p "Qual a URL completa do site (ex: https://example.com)?: " https_url
 read -p "Domínio para o escaneamento (ex: example.com): " url
-clear
+$c
 
 # Subdomínios
 read -p "Deseja buscar por subdomínios antes do scan? [S/N]: " resposta_sub
@@ -28,72 +28,72 @@ if [[ "$resposta_sub" =~ ^[Ss]$ ]]; then
   echo "Buscando subdomínios com dnsrato..."
   dnsrato "$url" /usr/share/wordlists/dnsrato/rato.txt > "$diretorio/subdomains.txt"
   echo "[**Busca de subdomínios concluída**]"
-clear
+$c
 
 else
   echo "[**Busca de subdomínios ignorada**]"
 fi
-clear
+$c
 
 
 # Nmap
 echo "[**Iniciando nmap!**]"
 sudo nmap -vv -sS -p-65535 -A -sV -Pn -T5 --script=all -g 53 -O --min-rate=450 -e -oN "$diretorio/scan-nmap.txt" "$url"
 echo "[**Nmap Finalizado**]"
-clear
+$c
 
 # Nuclei Paramsider
 echo "[**Iniciando nuclei paramsider!**]"
 sudo nf -d "$https_url" > "$diretorio/nuclei-vulns.txt"
 echo "[**Nuclei paramsider finalizado**]"
-clear
+$c
 
 # WhatWeb
 echo "[**Iniciando whatweb!**]"
 sudo whatweb -a3 "$https_url" > "$diretorio/services.txt"
 echo "[**WhatWeb finalizado**]"
-clear
+$c
 
 # Gobuster
 echo "Iniciando força bruta de diretórios com Gobuster!"
 sudo gobuster dir -u "$https_url" -w /usr/share/wordlists/dirb/common.txt > "$diretorio/directories.txt"
 echo "[**Gobuster finalizado**]"
-clear
+$c
 
 # Whois
 echo "[**Iniciando WhoIs!**]"
 sudo whois "$url" > "$diretorio/whois.txt"
 echo "[**WhoIs finalizado**]"
-clear
+$c
 
 # Nikto
 echo "[**Iniciando Nikto!**]"
 sudo nikto -url "$https_url" -C all > "$diretorio/scan-nikto.txt"
 echo "[**Nikto finalizado**]"
-clear
+$c
 
 # Nuclei
 echo "[**Iniciando o nuclei!**]"
 echo "$https_url" | subfinder -all | nuclei -severity low,medium,high,critical -t ~/.local/nuclei-templates > "$diretorio/nuclei-vulns.txt"
 echo "[**Scan do nuclei finalizado**]"
-clear
+$c
 
 # Criando info.txt
 echo "[**Criando arquivo info.txt**]"
 touch "$diretorio/info.txt"
 echo "[**Arquivo info.txt criado com sucesso!**]"
-clear
+$c
 
 # Scan do WordPress
 read -p "Deseja fazer scan do WordPress (caso exista)? [S/N]: " resposta_wp
 if [[ "$resposta_wp" =~ ^[Ss]$ ]]; then
   wpscan --url "$url" --force -e vp,vt,tt,cb,dbe,u,m --rua --api-token "$api" > "$diretorio/wp-scan.txt"
   echo "[**Scan do WordPress concluído**]"
-clear
+$c
 
 else
   echo "[**Scan do WordPress ignorado**]"
-clear
+$c
 
 fi
 
@@ -102,11 +102,11 @@ read -p "[**Deseja fazer download do site para análise?**] [S/N]: " resposta_do
 if [[ "$resposta_download" =~ ^[Ss]$ ]]; then
   wget -m "$https_url" -P "$diretorio/site-mirror"
   echo "[**Download do site concluído**]"
-clear
+$c
 
 else
   echo "[**Download do site ignorado**]"
-  clear
+$c
 
 fi
 
